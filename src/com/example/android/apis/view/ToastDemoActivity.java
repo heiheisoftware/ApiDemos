405be92d8f5abcc853b72c00aa7bf0f80fcf101b
heihei <heiheisoftware@gmail.com>
2014-03-08 23:32:40 +0800

    自定义Toast，演示同View高度齐平的Toast

--- src/com/example/android/apis/view/ToastDemoActivity.java
+++ src/com/example/android/apis/view/ToastDemoActivity.java
@@ -1,6 +1,7 @@
 package com.example.android.apis.view;
 
 import android.app.Activity;
+import android.content.Context;
 import android.graphics.Color;
 import android.graphics.Rect;
 import android.os.Bundle;
@@ -14,13 +15,13 @@ import com.example.android.apis.R;
 public class ToastDemoActivity extends Activity implements View.OnClickListener {
 
     private Button mBtnToast;
-    
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        
+
         setContentView(R.layout.toast_demo);
-        
+
         mBtnToast = (Button) findViewById(R.id.toast);
         mBtnToast.setOnClickListener(this);
     }
@@ -28,17 +29,37 @@ public class ToastDemoActivity extends Activity implements View.OnClickListener 
     @Override
     public void onClick(View v) {
         switch (v.getId()) {
-            case R.id.toast:
-//                  TODO: 自定义Toast位置
-                  Rect r = new Rect();
-                  mBtnToast.requestRectangleOnScreen(r, true);
-                  Toast t = Toast.makeText(this, "自定义Toast位置", Toast.LENGTH_LONG);
-                  t.setGravity(Gravity. CENTER_HORIZONTAL | Gravity.TOP, 0, r.top );
-                  t.getView().setBackgroundColor(Color.GRAY);
-                  t.show();
-                break;
-            default:
-                break;
+        case R.id.toast:
+            custToastAlignView(mBtnToast);
+            break;
+        default:
+            break;
         }
+    }
+
+    public static int getStatusBarHeight(Context c) {
+        int result = 0;
+        int resourceId = c.getResources().getIdentifier("status_bar_height",
+                "dimen", "android");
+        if (resourceId > 0) {
+            result = c.getResources().getDimensionPixelSize(resourceId);
+        } else {
+            result = c.getResources().getDimensionPixelSize(R.dimen.test_dimen_25);
+        }
+        return result;
+    }
+
+    /**
+     * 演示同View高度齐平的Toast
+     * @param v
+     */
+    private void custToastAlignView(View v) {
+        Rect r = new Rect();
+        v.requestRectangleOnScreen(r, true);
+        Toast t = Toast.makeText(this, "自定义Toast位置", Toast.LENGTH_LONG);
+        t.setGravity(Gravity.CENTER_HORIZONTAL | Gravity.TOP, 0, 
+                r.top - getStatusBarHeight(this)/* 这里面的Top是要剔除状态栏的高度 */);
+        t.getView().setBackgroundColor(Color.GRAY);
+        t.show();
     }
 }
