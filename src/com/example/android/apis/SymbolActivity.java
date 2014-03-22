e9a307805afac4fc8e43118b27ebfb82fcc50e22
heihei <heiheisoftware@gmail.com>
2014-03-22 09:23:03 +0800

    添加特殊符号，java format的百分号输出

---
+++ src/com/example/android/apis/SymbolActivity.java
@@ -0,0 +1,20 @@
+package com.example.android.apis;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class SymbolActivity extends Activity {
+
+    private TextView mTvSymbolDynamicPercent;
+    
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.symbol_activity);
+        
+        mTvSymbolDynamicPercent = (TextView) findViewById(R.id.symbol_dynamic_percent);
+        mTvSymbolDynamicPercent.setText(getString(R.string.symbol_dynamic_percent, 10));
+    }
+
+}
